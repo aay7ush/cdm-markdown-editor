82d2da900c54c86f335b192e4303c8a73d1d60c6
aay7ush <aay7ush@gmail.com>
2024-03-26 15:19:36 +0530

    add react context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
-const App = () => {
-	return <div>App</div>
-}
+import Editor from './components/Editor'
+import Preview from './components/Preview'
+import { MarkdownProvider } from './context/markdown'
+
+const App = () => (
+	<MarkdownProvider>
+		<main className="min-h-screen grid grid-rows-2 md:grid-rows-none md:grid-cols-2">
+			<Editor />
+			<Preview />
+		</main>
+	</MarkdownProvider>
+)
+
 export default App
