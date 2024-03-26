78f87356d8cb82af07868d3408d885948e12826d
aay7ush <aay7ush@gmail.com>
2024-03-26 15:23:37 +0530

    add starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,4 @@
-import Editor from './components/Editor'
-import Preview from './components/Preview'
-import { MarkdownProvider } from './context/markdown'
-
-const App = () => (
-	<MarkdownProvider>
-		<main className="min-h-screen grid grid-rows-2 md:grid-rows-none md:grid-cols-2">
-			<Editor />
-			<Preview />
-		</main>
-	</MarkdownProvider>
-)
-
+const App = () => {
+	return <h1>App</h1>
+}
 export default App
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,18 +1,4 @@
-import Markdown from 'react-markdown'
-import useMarkdown from '../context/markdown'
-
 const Preview = () => {
-	const [markdown] = useMarkdown()
-
-	return (
-		<section className="p-5 space-y-4">
-			<h4 className="text-3xl font-bold">Preview</h4>
-			<hr />
-			<Markdown className="markdown overflow-y-auto max-h-[80vh]">
-				{markdown}
-			</Markdown>
-		</section>
-	)
+	return <h2>Preview</h2>
 }
-
 export default Preview
